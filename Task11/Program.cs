8d078d91701a0ece95efd97d8e38ce73ecfb1f34
Anton S <geodesist.sushkov@gmail.com>
2021-12-06 23:38:09 +0300

    Добавьте файлы проекта.

---
+++ Task11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation x = new Equation { k = -15.2342, b = 23.3430 };
+            x.Root();
+            Console.ReadKey();
+        }
+        struct Equation
+        {
+            public double k, b;
+            public void Root()
+            {
+                Console.WriteLine("0=kx+b\nk={0}, b = {1}\nx1 = {2:f7}", k, b, -b / k);
+            }
+        }
+    }
+}
